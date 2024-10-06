cc2976d90bfbc81a6e19104da46145e1cf8a2d22
Torey Clark <clarktorey+github@gmail.com>
2024-10-06 17:55:15 -0500

    Secret Secrets

--- projects/functions/secret-secrets/02-dr-on/index.ts
+++ projects/functions/secret-secrets/02-dr-on/index.ts
@@ -1,2 +1,20 @@
-// Write your createAdvancedCipher function here! ✨
-// You'll need to export it so the tests can run it.
+export type Cipher = (character: string) => string
+
+export function createAdvancedCipher(onVowel: Cipher, onConsonant: Cipher, onPunctuation: Cipher) {
+    return (text: string) => {
+        const vowelPattern = /[aeiou]/i
+        const consonantPattern = /[bcdfghjklmnpqrstvwxyz]/i
+        let result = ``
+        for (const character of text) {
+            if (vowelPattern.test(character)) {
+                result = `${result}${onVowel(character)}`
+            } else if (consonantPattern.test(character)) {
+                result = `${result}${onConsonant(character)}`
+            } else {
+                result = `${result}${onPunctuation(character)}`
+            }
+        }
+
+        return result
+    }
+}
--- projects/functions/secret-secrets/03-the-golden-code/index.ts
+++ projects/functions/secret-secrets/03-the-golden-code/index.ts
@@ -1,2 +1,17 @@
-// Write your createCodeCracker function here! ✨
-// You'll need to export it so the tests can run it.
+export type Settings = {
+    attempts: number,
+    makeGuess: (text: string, attempt: number) => string,
+    validateGuess: (guess: string) => boolean,
+}
+export function createCodeCracker(settings: Settings) {
+    return (text: string) => {
+        for (let i = 0; i < settings.attempts; i++) {
+            const guess: string = settings.makeGuess(text, i)
+            if (settings.validateGuess(guess)) {
+                return guess
+            }
+        }
+
+        return undefined
+    }
+}
